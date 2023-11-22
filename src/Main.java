97a457b34d4c3bba54665723643d6a704579a2ce
Anton <anton22582258@gmail.com>
2023-11-22 19:46:25 +0300

    Task сделана

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println( " Практическая задача ");
+        String str = "123456789";
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            System.out.print(chars[i]);
+            if (i == (chars.length-1) / 2 ) {
+                System.out.print(" ");
+            }
+
+        }
     }
 }
